a4a7d258ffe57d0e7f374ea738d49d1466502e9a
mahfoud-mehalli <mahfoudmehalli12345@gmail.com>
2024-03-30 18:37:34 +0100

    delete post done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,9 +2,14 @@ import React, { useEffect, useState } from "react";
 import { getDocs, collection, deleteDoc, doc } from "firebase/firestore";
 import { auth, db } from "../firebase";
 
-const Home = () => {
+const Home = ({ isAuth }) => {
   const [postLists, setPostList] = useState([]);
   const postsCollectionRef = collection(db, "posts");
+  
+  const deletePost = async (id) => {
+    const postDoc = doc(db, "posts", id);
+    await deleteDoc(postDoc);
+  };
   
   useEffect(() => {
     const getPosts = async () => {
@@ -13,12 +18,8 @@ const Home = () => {
     };
 
     getPosts();
-  });
+  }, [deletePost]);
 
-  // const deletePost = async (id) => {
-  //   const postDoc = doc(db, "posts", id);
-  //   await deleteDoc(postDoc);
-  // };
 
   return (
     <div className="home">
@@ -26,8 +27,16 @@ const Home = () => {
         {postLists.map((post) => {
           return(
             <div className="post" key={post.id}>
-              <img src={post.selectedFile} alt="Post image" />
-              <h1>{post.title}</h1>
+              <img src={post.selectedFile} alt="Post Pic" />
+              <div className="title-delete">
+                <h1>{post.title}</h1>
+                {isAuth && post.author.id === auth.currentUser.uid && (
+                  <button className="deletePost" onClick={() => {deletePost(post.id);}}>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="#f4f4f4" d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6zM19 4h-3.5l-1-1h-5l-1 1H5v2h14z"/></svg>
+                    <p>Delete Post</p>
+                  </button>                  
+                )}
+              </div>
               <p>{post.description}</p>
               <h3>@{post.author.name}</h3>
             </div>
