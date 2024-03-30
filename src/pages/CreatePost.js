80952b5a58ab2fd38fb827f45722deae04a6d590
mahfoud-mehalli <mahfoudmehalli12345@gmail.com>
2024-03-30 02:34:14 +0100

    creating post done (with photo)

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -1,9 +1,57 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { TextField, Button } from '@material-ui/core';
+import { addDoc, collection } from "firebase/firestore";
+import { db, auth } from "../firebase";
+import { useNavigate } from "react-router-dom";
 
-const CreatePost = () => {
+function CreatePost({ isAuth }) {
+
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileUpload = (e) => {
+    setSelectedFile(e.target.files[0]); // Setting the selected file to state
+  };
+
+  const postsCollectionRef = collection(db, "posts");
+  let navigate = useNavigate();
+
+  const createPost = async () => {
+
+    const file = selectedFile ? selectedFile : null; // Handling case when file is not selected
+    const fileUrl = file ? URL.createObjectURL(file) : null; // Generating a URL for preview or other purposes
+    
+    try {
+      await addDoc(postsCollectionRef, {
+        title,
+        description,
+        selectedFile: fileUrl,
+        author: { name: auth.currentUser.displayName, id: auth.currentUser.uid },
+      });
+      navigate("/");
+    } catch (error) {
+      console.log(error)
+    }
+  };
+
+  useEffect(() => {
+    if (!isAuth) {
+      navigate("/login");
+    }
+  },);
+
   return (
-    <div>CreatePost</div>
-  )
+    <div className="createPostPage">
+      <div className="cpContainer">
+        <h1>Create A Post</h1>
+        <TextField onChange={(event) => {setTitle(event.target.value);}} variant="outlined" label="Title" fullWidth />
+        <TextField onChange={(event) => {setDescription(event.target.value);}} variant="outlined" label="Post Description" fullWidth multiline />
+        <input type="file" onChange={handleFileUpload} />
+        <Button onClick={createPost} variant="contained" size="large" type="submit" fullWidth>Create Post</Button>
+      </div>
+    </div>
+  );
 }
 
-export default CreatePost
+export default CreatePost;
